2c176ecd7c53f10f575be2ea87cfa80326f176c4
Alex Howard <alex-m-howard@pm.me>
2022-09-23 19:51:12 -0400

    Joke added to homepage

---
+++ Thyme2Eat/models/joke_model.py
@@ -0,0 +1,23 @@
+from ..db import db
+
+
+class Joke(db.Model):
+    """ 
+            Joke MODEL
+    ID         : Primary Key
+    Text      : Text
+    """
+    __tablename__ = 'jokes'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    text = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    def __repr__(self):
+        return f"<Joke #{self.id}: {self.text}>"
