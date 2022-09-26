887f4db6e37178d1bc803c16df4ebc72aac249f0
Alex-Howard <alex-m-howard@pm.me>
2022-09-26 17:02:59 -0400

    Login and global user scope added

--- Thyme2Eat/routes/user_routes/user_routes.py
+++ Thyme2Eat/routes/user_routes/user_routes.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, render_template, flash, redirect, request, url_for, session, g
+from flask import Blueprint, render_template, flash, redirect, url_for, session, g
 from sqlalchemy.exc import IntegrityError
 
 from ...models.user_model import User
 from ...models.joke_model import Joke
 
 from .forms import SignupForm, LoginForm
+
+from ..meal_routes.meal_routes import app_meal
 
 from ...db import db
 
@@ -24,8 +26,7 @@ app_user = Blueprint(
 @app_user.route('/')
 def home():
     """ Show Logo if not logged in, otherwise User profile"""
-    return render_template('/home.html')
-    #return redirect(url_for('app_user.login_page'))
+    return render_template('/home.html')   
 
 @app_user.route('/signup', methods=["GET", "POST"])
 def signup_page():
@@ -55,13 +56,12 @@ def signup_page():
             print(err)
             return render_template('/sign_up.html', form=form)
 
-        do_login(user.id)    
+        do_login(user)    
         return redirect(url_for('app_user.home'))
 
     else:
         return render_template('/sign_up.html', form=form)
-        
-        
+               
 @app_user.route('/login', methods=["GET", "POST"])
 def login_page():
     """ Show login form """
@@ -71,28 +71,21 @@ def login_page():
     
     form = LoginForm()
     
-    # if form.validate_on_submit():
-    #     try:
+    if form.validate_on_submit():
+        user = User.authenticate(
+            username = form.username.data,
+            password = form.password.data
+        )
             
-
-    #     except IntegrityError:
-    #         flash("Username/Email already taken", 'danger')
-    #         print('integrity error')
-    #         return render_template('users/signup.html', form=form)
-        
-    #     except Exception as err:
-    #         flash("Unknown error has occurred. Try again later")
-    #         print(err)
-    #         return render_template('users/signup.html', form=form)
-
-    #     finally:
-    #         do_login(user.id)    
-    #         return redirect(url_for('app_user.home'))
-
-    # else:
+        if user:
+            do_login(user)
+            flash(f"Hello, {user.username}!", "success")
+            return redirect(url_for('app_user.home'))
+        else:
+            flash("Invalid Credentials.", "danger")
+            
     return render_template('/login.html', form=form)
     
-
 @app_user.route('/logout', methods=["GET"])
 def logout_user():
     """Initiate Logout"""
@@ -101,11 +94,8 @@ def logout_user():
     
     return redirect(url_for('app_user.home'))
     
-
-
 @app_user.route('/<int:user_id>/profile')
 def show_profile(user_id):
-    print(user_id)
     print(session)
     return render_template('/profile.html')
 
@@ -115,23 +105,26 @@ def show_profile(user_id):
 
 
 
-# Handle logging in/out of user
+
 @app_user.before_request
+@app_meal.before_request
 def add_user_globally():
-    """If user is logged in, add user_id to Flask global"""
+    """If user is logged in, add user_id to Flask global for app_meal and app_user"""
     
     if CURRENT_USER_ID in session:
         g.user = User.query.get(session[CURRENT_USER_ID])
     else:
-        g.user = None     
+        g.user = None
         
-def do_login(user_id):
+def do_login(user):
     """ Log in user """
     
-    session[CURRENT_USER_ID] = user_id
+    session[CURRENT_USER_ID] = user.id
     
 def do_logout():
     """ Log out current user """
     
     if CURRENT_USER_ID in session:
         del session[CURRENT_USER_ID]
+        
+    
