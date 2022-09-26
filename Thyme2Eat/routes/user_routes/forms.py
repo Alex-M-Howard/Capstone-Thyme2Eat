2979a50da30b29b32cc6ea6ebf9af7d6757ce064
Alex-RaspPi <alex-m-howard@pm.me>
2022-09-26 10:24:28 -0400

    Added length requirements to username and pass

--- Thyme2Eat/routes/user_routes/forms.py
+++ Thyme2Eat/routes/user_routes/forms.py
@@ -7,14 +7,14 @@ from wtforms.validators import Email, InputRequired, Length
 class SignupForm(FlaskForm):
     """Form for a user to sign-up"""
 
-    username = StringField("Username", validators=[InputRequired(message='You must enter a username'), Length(min=6)])
+    username = StringField("Username", validators=[InputRequired(message='You must enter a username'), Length(min=6, max=20)])
     email = StringField("Email", validators=[InputRequired(), Email(granular_message=True)])
-    password = PasswordField("Password", validators=[InputRequired(message='You must enter a password')])
+    password = PasswordField("Password", validators=[InputRequired(message='You must enter a password'), Length(min=6)])
     
 
 class LoginForm(FlaskForm):
     """Form for user to login"""
     
     username = StringField("Username", validators=[InputRequired(message='You must enter a username')])
-    password = PasswordField("Password", validators=[InputRequired(message='You must enter a password')])
+    password = PasswordField("Password", validators=[InputRequired(message='You must enter a password'), Length(min=6, message="Password must be at least 6 characters in length")])
 
