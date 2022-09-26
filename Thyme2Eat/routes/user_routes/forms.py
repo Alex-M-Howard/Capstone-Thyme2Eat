7a540f6a44c37e7b0dcdd68b252fb0796eaee95f
Alex-RaspPi <alex-m-howard@pm.me>
2022-09-26 09:45:03 -0400

    Bug - Validator wasn't inside validator param

--- Thyme2Eat/routes/user_routes/forms.py
+++ Thyme2Eat/routes/user_routes/forms.py
@@ -9,12 +9,12 @@ class SignupForm(FlaskForm):
 
     username = StringField("Username", validators=[InputRequired(message='You must enter a username'), Length(min=6)])
     email = StringField("Email", validators=[InputRequired(), Email(granular_message=True)])
-    password = PasswordField("Password", InputRequired(message='You must enter a password'))
+    password = PasswordField("Password", validators=[InputRequired(message='You must enter a password')])
     
 
-class LoginForm(ModelForm):
+class LoginForm(FlaskForm):
     """Form for user to login"""
     
     username = StringField("Username", validators=[InputRequired(message='You must enter a username')])
-    password = PasswordField("Password", InputRequired(message='You must enter a password'))
+    password = PasswordField("Password", validators=[InputRequired(message='You must enter a password')])
 
