defa091229f2a1677c6dac9653b125495717c590
Alex-Howard <alex-m-howard@pm.me>
2022-09-27 19:09:06 -0400

    Formatting and add SaveRecipe Event Listener

--- Thyme2Eat/static/js/app.js
+++ Thyme2Eat/static/js/app.js
@@ -1,7 +1,21 @@
 SPOONACULAR_API_KEY = '25bf790109054f9387a17986d94ebcfb'
 BASE_RECIPES_URL = "https://api.spoonacular.com/recipes";
 
-NUMBER_OF_RESULTS = 24
+NUMBER_OF_RANDOM_RESULTS = 32
+
+/*************************************
+ *  
+ *  Functionality for nav-bar Hamburger button
+ * 
+ */
+$(document).ready(function () {
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function () {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
+});
 
 
 /*************************************
@@ -10,14 +24,15 @@ NUMBER_OF_RESULTS = 24
  *  Prevent Default Button Function
  *  Get random meals
  *  Show meals on page
+ *  Add Event Listeners to SAVE buttons
  * 
  */
-
 $('#random-meal-search').on('click', (async (event) => {
     event.preventDefault()
 
     let meals = await getRandomMeals();
     showMeals(meals);
+    saveRecipeEvent();
 }))
 
 
@@ -28,13 +43,12 @@ $('#random-meal-search').on('click', (async (event) => {
  *  Returns an array of objects
  * 
  */
-
 const getRandomMeals = async () => {
     let diet = $("#diet").val();
     let type = $("#type").val();
     let intolerances = $("#intolerances").val();
     let apiKey = SPOONACULAR_API_KEY;
-    let number = NUMBER_OF_RESULTS;
+    let number = NUMBER_OF_RANDOM_RESULTS;
     let sort = "random";
     let instructionsRequired = "true";
 
@@ -63,7 +77,6 @@ const getRandomMeals = async () => {
  *  If user is logged in, create SAVE button
  * 
  */
-
 const showMeals = (meals) => {
     
     for (meal of meals) {
@@ -86,13 +99,13 @@ const showMeals = (meals) => {
             </div>
         `);
             
+      // If user logged in, create SAVE buttons
       if (user !== 'None') {
         $('div.card-header-title:last').html(
           $('div.card-header-title:last').html() +                 
-                `
-                <form action='{{ url_for("app_user.save_recipe", user_id=g.user.id) }}'method="POST">
-                  <button class="button is-primary" type="submit">Save Recipe</button>
-                </form>`              
+              `
+                <button class="button is-primary save-recipe" data-meal_id=${meal.id}  type="submit">Save Recipe</button>
+              `              
           );
       } 
       
@@ -101,11 +114,22 @@ const showMeals = (meals) => {
     }
 }
 
-$(document).ready(function () {
-  // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function () {
-    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-    $(".navbar-burger").toggleClass("is-active");
-    $(".navbar-menu").toggleClass("is-active");
-  });
-});
+
+/*************************************
+ * 
+ *  Add event listener for SAVE buttons
+ *  Prevent Default Button Function
+ *  Call USER route to Save/Remove Meal
+ * 
+ */
+const saveRecipeEvent = () => {
+  $('.save-recipe').on('click', (async (event) => {
+    event.preventDefault()
+   
+    let mealId = $(event.target).data('meal_id')
+
+    const response = await axios.post(`/meals/${mealId}/save`);
+
+    return response.data.results;
+  }))
+}
