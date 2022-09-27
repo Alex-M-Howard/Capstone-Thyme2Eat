2986711da4009d984c5cd7758aa94c55e22ac87d
Alex-Howard <alex-m-howard@pm.me>
2022-09-27 19:10:12 -0400

    Working on Saving Meal to Favorites

--- Thyme2Eat/routes/meal_routes/meal_routes.py
+++ Thyme2Eat/routes/meal_routes/meal_routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, flash, redirect, request, url_for, g
 
+from ...models.meal_model import Meal
+
 import requests
 
 SPOONACULAR_API_KEY = '25bf790109054f9387a17986d94ebcfb'
@@ -66,3 +68,31 @@ def show_recipe(meal_id):
 
     return render_template('show_recipe.html', recipe=recipe.json(), nutrition=nutrition.url)
     
+@app_meal.route('/<int:meal_id>/save', methods=["POST"])
+def save_recipe(meal_id):
+    """ Save recipe to User's recipe book"""
+    
+    params = {
+        "apiKey": SPOONACULAR_API_KEY,
+        "id": meal_id
+    }
+
+    meal = requests.get(f"{RECIPES_API}/{meal_id}/{INFO}", params)
+    meal = meal.json()
+    
+    
+    # TODO --> Check if meal in database, or add meal
+    new_meal = Meal(
+        id=meal.id, 
+        title=meal.title, 
+        image_url=meal.image, 
+        servings=meal.servings, 
+        time=meal.readyInMinutes, 
+        diets=meal.diets, 
+        instructions = meal.analyzedInstructions,
+        meal_type=meal.dishTypes)
+    
+    db.session.add(new_meal)
+    db.session.commit()
+    
+    return redirect(url_for('app_meal.get_random_meal'))
