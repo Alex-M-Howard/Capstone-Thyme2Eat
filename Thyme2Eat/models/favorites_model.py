2dc37b57555e04cdc117adfc3339606089dd9e31
Alex-RaspPi <alex-m-howard@pm.me>
2022-09-29 11:45:09 -0400

    Added Favorite __repr__

--- Thyme2Eat/models/favorites_model.py
+++ Thyme2Eat/models/favorites_model.py
@@ -16,3 +16,9 @@ class Favorite(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='cascade'))
 
     meal_id = db.Column(db.Integer, db.ForeignKey('meals.id', ondelete='cascade'), unique=True)
+
+
+    def __repr__(self):
+        """ Return string that shows meal id and user id for JS AJAX """
+        
+        return f"<Favorite {self.id} - user_id:{self.user_id} - meal_id:{self.meal_id}"
