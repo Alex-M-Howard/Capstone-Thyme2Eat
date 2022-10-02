06728e7be67a4173299308e488bfe0ab061f2907
Alex-Howard <alex-m-howard@pm.me>
2022-10-02 19:15:13 -0400

    Changed tasks to .PY for TODO Tree Highlighting

---
+++ TASKS.py
@@ -0,0 +1,21 @@
+# DONE - If user is logged in, show a favorite or save button
+# DONE - Clicking button sends axios request to python to save that meal in database   
+# DONE - Clicking button again, if exists -> remove from database                       
+# DONE - Route to see saved recipes: Sort by meal type (breakfast, lunch, dinner)
+# DONE - Update navbar layout so not cutoff on small screen                             
+
+# TODO - Add search box to navbar -> use autocomplete
+
+# TODO - Add profile pic option?                                                        
+
+# TODO - Font awesome for loading on random meals? --> Loads so quickly, is it necessary?                                       
+
+# TODO - User profile: Meal Plan, Favorites
+
+# TODO - When looking at a recipe, see similar?                                         
+
+# TODO - Can we make a printable version like a real recipe site?                       
+
+# TODO - User logged in api route
+
+#TODO - Add Remove button for recipes while in your favorites
