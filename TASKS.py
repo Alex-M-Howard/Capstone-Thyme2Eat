b04db860e77d0a1a01f033bdef7253f9c094d26b
Alex Howard <alex-m-howard@pm.me>
2022-10-06 18:15:35 -0400

    updated tasks

--- TASKS.py
+++ TASKS.py
@@ -22,3 +22,28 @@
 # Ran out of points very quickly today. Need to figure out point values for each endpoint I have
 # Complex search is the reason why --> limit search results
 
+
+
+# Added in since last week
+'''
+ Font awesome
+ Favorites Page with tabs
+ Split JS further to match pages
+ Can favorite/unfavorite recipes now
+ Print Page version of recipe
+ Removed brunch - added snacks
+ Flash notifications on signup/login/logout pages
+ Search function
+ Similar recipe when viewing
+ Currently trying to add in meal plan
+ 
+ Ask about consistency with backend vs frontend -> Should I change it now or keep it in mind for next time?
+
+'''
+#  import json
+#  json.dumps()
+#  json.loads()
+
+# >>> import sys
+# >>> sys.path.append('.')
+# >>> import Thyme2Eat
---
+++ Thyme2Eat/__main__.py
@@ -0,0 +1,2 @@
+import Thyme2Eat
+
