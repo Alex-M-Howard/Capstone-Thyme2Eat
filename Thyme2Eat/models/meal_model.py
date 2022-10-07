152c308a6e2f9a2f86722a7a689746a2cf913947
Alex Howard <alex-m-howard@pm.me>
2022-10-07 12:42:47 -0400

    Using analyzed steps and saving nutrition url

--- Thyme2Eat/models/meal_model.py
+++ Thyme2Eat/models/meal_model.py
@@ -6,50 +6,28 @@ from ..models.favorites_model import Favorite
 class Meal(db.Model):
     """ 
             MEAL MODEL
-    ID         : Primary Key
-    Title      : Text
-    Image URL  : Text 
-    Servings   : Integer
-    Time       : Float
-    Diets      : Text
+    ID                   : Primary Key
+    Title                : Text
+    Image URL            : Text 
+    Servings             : Integer
+    Time                 : Float
+    Diets                : Text
+    analyzedInstructions : Text -> JSON string
+    nutrition_url        : Text
     """
 
     __tablename__ = 'meals'
 
-    id = db.Column(
-        db.Integer,
-        primary_key=True,
-    )
-
-    title = db.Column(
-        db.Text,
-        nullable=False,
-    )
-
-    image_url = db.Column(
-        db.Text,
-    )
-
-    servings = db.Column(
-        db.Integer,
-        nullable=False,
-    )
-    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text)
+    servings = db.Column(db.Integer, nullable=False)
+    nutrition_url = db.Column(db.Text)
     analyzedInstructions = db.Column(db.Text)
-    
     meal_type = db.Column(db.Text,)
-
-    time = db.Column(
-        db.Float,
-        nullable=False,
-    )
-    
-    diets = db.Column(
-        db.Text,
-        nullable=False,
-    )
+    time = db.Column(db.Float, nullable=False,)
+    diets = db.Column(db.Text, nullable=False,)
     
     
-  
     def __repr__(self):
         return f"<Meal #{self.id}: {self.title}>"
