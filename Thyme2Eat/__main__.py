580241bad8ad38ef3e4d76b466f3120aff44651d
Alex Howard <99138808+Alex-M-Howard@users.noreply.github.com>
2022-10-11 08:30:41 -0400

    Update __main__.py

--- Thyme2Eat/__main__.py
+++ Thyme2Eat/__main__.py
@@ -1,5 +1,6 @@
 from . import *
+import os
 
 if __name__ == '__main__':
   app = create_app()
-  app.run(host="0.0.0.0", port=8000, debug=False)
+  app.run(host="0.0.0.0", port=os.environ.get('$PORT') , debug=False)
