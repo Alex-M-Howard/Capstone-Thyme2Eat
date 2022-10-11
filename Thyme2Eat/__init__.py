1cc28bf97355616dd26a43af5b9b61eb0f2a018b
Alex Howard <99138808+Alex-M-Howard@users.noreply.github.com>
2022-10-11 08:16:43 -0400

    Update __init__.py

--- Thyme2Eat/__init__.py
+++ Thyme2Eat/__init__.py
@@ -21,7 +21,7 @@ def create_app():
     app = Flask(__name__)
     
     # Configure Setup
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///thyme2eat"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL') #"postgresql:///thyme2eat"
     
     # Windows Database URI
     #app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://Alex:273gyuec32@localhost:5432/thyme2eat"
